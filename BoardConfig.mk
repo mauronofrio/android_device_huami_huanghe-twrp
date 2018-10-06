8ddb6341c1f732e6ea7867f3e068120123fb74f7
mauronofrio matarrese <poronofrio@gmail.com>
2018-10-06 12:59:33 +0200

    TWRP-ify

---
+++ BoardConfig.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := m200
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := m200
+TARGET_BOARD_PLATFORM_GPU := vivante-gc1000
+
+# Architecture
+TARGET_ARCH := mips
+TARGET_ARCH_VARIANT := mips32r2-fp-xburst
+TARGET_CPU_ABI := mips
+TARGET_CPU_ABI2 := mips
+TARGET_CPU_VARIANT := mips
+
+# Kernel
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE        := 0x10000000
+BOARD_KERNEL_PAGESIZE    := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
+TARGET_PREBUILT_KERNEL := device/huami/huanghe/kernel
+
+# Partitions
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 9216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16384
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 843776
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2768896 # (58846081024-16384)
+BOARD_CACHEIMAGE_PARTITION_SIZE := 102400
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Recovery
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_THEME := watch_mdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_IGNORE_MISC_WIPE_DATA := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_SCREEN_BLANK_ON_BOOT := true
