d049e22e99b891c85588adb92ad9e17f6fa338f5
mauronofrio matarrese <poronofrio@gmail.com>
2018-12-01 13:54:40 +0100

    Update fingerprint for us version

--- omni_huanghe.mk
+++ omni_huanghe.mk
@@ -34,7 +34,7 @@ TARGET_VENDOR_DEVICE_NAME := huanghe
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=watch \
     PRODUCT_NAME=huanghe \
-    BUILD_FINGERPRINT=Huami/huanghe/watch:5.1/LMY47D/67:user/release-keys 
+    BUILD_FINGERPRINT=Huami/huangheUS/watch:5.1/LMY47D/18:user/release-keys  # use Huami/huanghe/watch:5.1/LMY47D/67:user/release-keys  for the chinese one
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.build.huami.model=A1612 \ # use A1602 for the chinese one
